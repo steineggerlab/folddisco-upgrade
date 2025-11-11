90e4afc7675131f262cf876c06f5ea93e98b1c87
jwyoon05 <jwyoon05@snu.ac.kr>
2025-11-11 14:54:38 +0900

    Remove old nested folddisco-upgrade path

--- folddisco-upgrade/folddisco-validation-result/command/Rtest.R
+++
@@ -1,4 +0,0 @@
-g1 <- scan("folddisco-upgrade/folddisco-validation-result/result/group_lowe_querylen.txt")
-g2 <- scan("folddisco-upgrade/folddisco-validation-result/result/group_highe_querylen.txt")
-t.test(g1, g2, var.equal = FALSE, alternative = "greater")
-wilcox.test(g1, g2)   # Mann–Whitney
