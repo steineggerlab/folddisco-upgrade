776b4f1c8caf63422111147b98cb49d61a92a851
jwyoon05 <jwyoon05@snu.ac.kr>
2025-12-04 16:28:40 +0900

    Error

--- command/00_file_search.py
+++
@@ -1,8 +0,0 @@
-import os
-
-ROOT = "result/folddisco_results_raw"
-TARGET = "output_1twfB04.txt"
-
-for root, dirs, files in os.walk(ROOT):
-    if TARGET in files:
-        print("FOUND:", os.path.join(root, TARGET))
